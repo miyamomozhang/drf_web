001783e70147bf22f3867b94c4bafe3ac15d701e
miyamomozhang <zhangxiaoyi_miya@163.com>
2023-10-25 14:41:34 +0800

    article的增删改查，自己实现，非DRF提供的Mixin类

--- article/serializers.py
+++ article/serializers.py
@@ -3,10 +3,30 @@
 
 from rest_framework import serializers
 
+# 序列化器 和 我们之前的 Model 很像，代码重复，ModelSerializer简化
+from .models import Article
 
+"""
 class ArticleListSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(allow_blank=True, max_length=200)
     body = serializers.CharField(allow_blank=True)
     create_time = serializers.DateTimeField()
     update_time = serializers.DateTimeField()
+"""
+
+
+# 序列化器 和 我们之前的 Model 很像，代码重复，ModelSerializer简化
+class ArticleListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+        # fields = '__all__'代表要使用所有的字段
+
+        # 只使用一部分字段，就用下面的方式
+        # fields = [
+        #     "id",
+        #     "title",
+        #     "create_time",
+        #     # "update_time",
+        # ]
--- article/views.py
+++ article/views.py
@@ -1,11 +1,63 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from .models import Article
 from .serializers import ArticleListSerializer
-
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
+# django的返回方式
+"""
 def article_list(request):
     articles = Article.objects.all()
     # 序列化
     serializer = ArticleListSerializer(articles, many=True)  # many=True 时传入的参数需要包含多个对
     return JsonResponse(serializer.data, safe=False)
+"""
+
+
+# 不写请求方式的话，默认只支持GET请求
+# FBV, django rest_framework
+@api_view(["GET", "POST"])
+def article_list(request):
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleListSerializer(articles, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ArticleListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+
+
+# CBV，django rest_framework
+class ArticleDetail(APIView):
+    # 单个文章详情页
+    def get_object(self, pk):
+        # pk即prime key, 主键，唯一
+        # 获取 pk 对应的文章对象
+        try:
+            return Article.objects.get(pk=pk)
+        except:
+            raise Http404
+
+    def get(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleListSerializer(article)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        article = self.get_object(pk)
+        serializer = ArticleListSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        article = self.get_object(pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
